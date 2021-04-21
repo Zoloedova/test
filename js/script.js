8b6c1a1fc9cc936347a74f3b7fe7d1901a21ff56
Zoloedova <82142822+Zoloedova@users.noreply.github.com>
2021-04-21 22:58:27 +0300

    задача 45

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let str = ("aaa bbb ccc");
+
+str =  str.substr(0,4) + str.substr(7,4);
+alert(str);*/
+
+
+/*let str = ("aaa bbb ccc");
+str=  str.substring (0,3) + str.substring (7,11);
+alert(str);*/
+
+
+/*let str = ("aaa bbb ccc");
+str=  str.slice(4,7);
+alert(str);
